a03e6b09956c6804673075145cd4cbc9c27d2127
amirdafnaiaquant <amird.aquant.io>
2025-01-15 20:17:02 +0200

    vibrates less

--- src/KeyBoard.js
+++ src/KeyBoard.js
@@ -21,7 +21,7 @@ export function MyKeyBoard({ handleKeyDown, buttonTheme }) {
 
     // Add feedback: vibration and highlight
     if (navigator.vibrate) {
-      navigator.vibrate(50); // Vibrate for 50ms
+      navigator.vibrate(25); // Vibrate for 50ms
     }
     setPressedKey(button);
 
