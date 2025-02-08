dd675f398a1632d4442e0f934a17b407872bfaf1
amirdafnaiaquant <amird.aquant.io>
2025-02-08 16:23:51 +0200

    more css

--- src/Timer.js
+++ src/Timer.js
@@ -5,12 +5,12 @@ import { GAMESTATUS } from "./Consts";
 
 const getFixedPercentage = (numerator, denomeneator) => {
   const percentage = (numerator / denomeneator) * 100;
-  const fixedPercentage = percentage.toFixed(0) 
+  const fixedPercentage = percentage.toFixed(0);
   return fixedPercentage;
 };
 
-const getHMSFormat = (timeInSeconds_) =>{
-  const timeInSeconds = Math.round(timeInSeconds_)
+const getHMSFormat = (timeInSeconds_) => {
+  const timeInSeconds = Math.round(timeInSeconds_);
   const hours = Math.floor(timeInSeconds / 3600);
   const minutes = Math.floor((timeInSeconds % 3600) / 60);
   const seconds = timeInSeconds % 60;
@@ -25,25 +25,24 @@ const getHMSFormat = (timeInSeconds_) =>{
           "0"
         )}`;
   return formattedTime;
-
-
-
-}
+};
 
 const getTimeToSolve = (start, end) => {
-  const diffInSeconds = Math.round((end-start) / 1000);
-  const formattedTime =getHMSFormat(diffInSeconds)
+  const diffInSeconds = Math.round((end - start) / 1000);
+  const formattedTime = getHMSFormat(diffInSeconds);
   return formattedTime;
 };
 
 const Top = ({ word, riddle, gameStatus }) => {
-  const text = gameStatus === GAMESTATUS.win  ? "הצלחת!" : "לא נורא..."
+  const text = gameStatus === GAMESTATUS.win ? "הצלחת!" : "לא נורא...";
   const timeToSolve = getTimeToSolve(riddle.startTime, riddle.endTime);
   return (
     <>
       {" "}
       {/* Header Section */}
-      <h2 className="win-message" dir="rtl">{text}</h2>
+      <h2 className="win-message" dir="rtl">
+        {text}
+      </h2>
       <div className="word-display">
         {word.map((letter, index) => (
           <div key={index} className="letter-tile">
@@ -52,7 +51,8 @@ const Top = ({ word, riddle, gameStatus }) => {
         ))}
       </div>
       {/* Time to Solve */}
-      <div className="solve-label stat-label"> {timeToSolve} זמן פתרון    </div>
+      <div className="stat-large"> {timeToSolve} </div>
+      <div className="stat-label"> זמן פתרון </div>
     </>
   );
 };
@@ -66,7 +66,9 @@ const PersonalStats = () => {
   return (
     <div className="stats-container personal-stats">
       <div className="stat">
-        <div className="stat-large">{getHMSFormat(personalStats.best_time)}</div>
+        <div className="stat-large">
+          {getHMSFormat(personalStats.best_time)}
+        </div>
         <div className="stat-label">הזמן הקצר ביותר שלך</div>
       </div>
       <h3 className="personal-title">המשחקים שלך</h3>
@@ -76,7 +78,6 @@ const PersonalStats = () => {
           {personalStats.wins} / {personalStats.total} הצלחות
         </div>
       </div>
-      
     </div>
   );
 };
@@ -92,7 +93,9 @@ const GlobalStats = () => {
     <>
       <div className="stats-container global-stats">
         <div className="stat">
-          <div className="stat-large">{getHMSFormat(globalStats.best_time)}</div>
+          <div className="stat-large">
+            {getHMSFormat(globalStats.best_time)}
+          </div>
           <div className="stat-label">
             הזמן הקצר ביותר <br /> {globalStats.user_name}
           </div>
@@ -104,7 +107,6 @@ const GlobalStats = () => {
             {globalStats.total_wins} / {globalStats.total_plays} הצליחו
           </div>
         </div>
-        
       </div>
     </>
   );
@@ -138,14 +140,14 @@ const Timer = (onTimeEnds) => {
   }
   return (
     <>
-      <div className="timer">
-        <h3 className="time">החידה הבאה בעוד</h3>
-        <div className="time">
-          {timeLeft.hours.toString().padStart(2, "0")}:
-          {timeLeft.minutes.toString().padStart(2, "0")}:
-          {timeLeft.seconds.toString().padStart(2, "0")}
-        </div>
-      </div>
+      <h3 className="time">
+        {`           החידה הבאה בעוד ${timeLeft.hours
+          .toString()
+          .padStart(2, "0")}:${timeLeft.minutes
+          .toString()
+          .padStart(2, "0")}:${timeLeft.seconds.toString().padStart(2, "0")}
+          `}
+      </h3>
     </>
   );
 };
@@ -153,13 +155,13 @@ const Timer = (onTimeEnds) => {
 const Stats = () => {
   return (
     <>
-      <PersonalStats />
       <GlobalStats />
+      <PersonalStats />
     </>
   );
 };
 
-const GuestUserMessage = ({login}) => {
+const GuestUserMessage = ({ login }) => {
   return (
     <>
       <h3> ...אם היית מחובר היו כאן סטטיסטיקות</h3>
@@ -185,7 +187,7 @@ const TimerToMidnight = ({
           ✖
         </button>
         <Top word={riddle.solution} riddle={riddle} gameStatus={gameStatus} />
-        {isLoggedIn ? <Stats /> : <GuestUserMessage login={login}/>}
+        {isLoggedIn ? <Stats /> : <GuestUserMessage login={login} />}
         <Timer onTimeEnds={onTimeEnds} />
       </div>
     </div>
