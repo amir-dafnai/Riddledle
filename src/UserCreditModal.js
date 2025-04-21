414e0fa997c8547d32549e8a449526baef24ab8e
amirdafnaiaquant <amird.aquant.io>
2025-04-21 09:22:20 +0300

    changed text of credit modal

--- src/UserCreditModal.js
+++ src/UserCreditModal.js
@@ -9,8 +9,7 @@ export const CreditModal = ({ onClose }) => {
         </button>
         <h2 dir="rtl"> איזה כיף! 🎉</h2>
         <p>
-          החידה של היום חידה שאתה יצרת! אמנם לא תוכל להתחרות אבל בוא נראה מה
-          חברים שלך חושבים עליה!
+          החידה של היום היא חידה שלך! קדימה להשוויץ לחברים!
         </p>
         <h4>לשיתוף בוואטסאפ</h4>
         <WhatsAppShareButton message={"בואו לפתור חידה שאני כתבתי!"} />
