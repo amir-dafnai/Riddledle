e8abd0eded663789bce5bbf41e940181ef8d0339
amirdafnaiaquant <amird.aquant.io>
2025-06-26 23:19:18 +0300

    new welcome modal

--- src/WelcomeModal.js
+++ src/WelcomeModal.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./WelcomeModal.css";
 
-const WelcomeHeadline = ({isMultiRiddle}) => {
+const WelcomeHeadline = ({ isMultiRiddle }) => {
   const multiRiddleheadline1 = "האתגר השבועי!";
   const multiReadleHeadline2 = "נסו לפתור חמש חידות בדקה";
-  const headLineText = !isMultiRiddle ? "יאללה חידה חדשה! 🎉" : multiRiddleheadline1;
+  const headLineText = !isMultiRiddle
+    ? "יאללה חידה חדשה! 🎉"
+    : multiRiddleheadline1;
 
   return (
     <>
@@ -14,49 +16,54 @@ const WelcomeHeadline = ({isMultiRiddle}) => {
   );
 };
 
+const HowToPLay = ({ onClick }) => {
+  return (
+    <button dir="rtl" className="how-to-play-button" onClick={onClick}>
+      איך משחקים?
+    </button>
+  );
+};
+
+const LoginButton = ({ login }) => {
+  return (
+    <div className="button-container">
+      <button className="action-button" onClick={login}>
+        התחבר
+      </button>
+    </div>
+  );
+};
+
+const ContinueButton = ({ onClose }) => {
+  return (
+    <div className="button-container">
+      <button className="action-button" onClick={onClose}>
+        המשך לחידה
+      </button>
+    </div>
+  );
+};
+
 export const WelcomeModal = ({
   onClose,
   isLoggedIn,
   login,
   onHowToPLay,
-  isMultiRiddle,
+  isMultiRiddl_e,
 }) => {
-  const continueButtonText = isLoggedIn
-    ? "המשך לחידה!"
-    : !isMultiRiddle
-    ? "המשך כאורח"
-    : "חייבים להתחבר כדי לשחק היום";
-  const loginText = !isMultiRiddle ? "כדאי להתחבר כדי להיות חלק מהתחרות!" : "";
-  const canPlay = !isMultiRiddle || isLoggedIn;
+  const isMultiRiddle = true;
   return (
     <div className="welcome-modal-overlay">
       <div className="welcome-modal">
         <h1 className="unclickable">Riddledle</h1>
-        <WelcomeHeadline isMultiRiddle={isMultiRiddle}/>
-        <button dir="rtl" className="how-to-play-button" onClick={onHowToPLay}>
-          איך משחקים?
-        </button>
-
-        {!isLoggedIn && (
-          <div dir="rtl" className="login-section">
-            <p dir="rtl" className="login-hint">
-              {loginText}
-            </p>
-            <span className="login-link" onClick={login}>
-              להתחברות
-            </span>
-          </div>
+        <WelcomeHeadline isMultiRiddle={isMultiRiddle} />
+        {isLoggedIn ? (
+          <ContinueButton onClose={onClose} />
+        ) : (
+          <LoginButton login={login} />
         )}
-        {
-          <button
-            className="continue-button"
-            dir="rtl"
-            onClick={onClose}
-            disabled={!canPlay}
-          >
-            {continueButtonText}
-          </button>
-        }
+
+        <HowToPLay onClick={onHowToPLay} />
       </div>
     </div>
   );
