b30ccd3ebbb0bbfeb7f1eebec4248199765803d5
amirdafnaiaquant <amird.aquant.io>
2026-05-26 21:50:15 +0300

    fixed typo

--- src/RecordsBreak.js
+++ src/RecordsBreak.js
@@ -4,7 +4,7 @@ import {
 } from "./localStorageUtils";
 
 const brokeBothText = `שברת שיא אישי וגם את השיא היומי!🎉`;
-const brokePersonalText = `שברת שיא אישששי!🎉`;
+const brokePersonalText = `שברת שיא אישי!🎉`;
 const brokeGlobalText = `שברת את השיא היומי!🎉`;
 
 const getBestTimeGlobal=(globalStats)=>{
